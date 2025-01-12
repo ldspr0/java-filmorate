9ff7094484c72318a7eda02e4c24f3c4d0f7878a
Al3x Ivanov <pro87@yandex.ru>
2025-01-12 23:38:44 +0400

    Added logging feature

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
@@ -12,8 +13,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private final Map<Long, Film> films = new HashMap<>();
+    private static final String MSG_RELEASEDATE_VALIDATION_ERROR = "Дата релиза фильма не может быть раньше 28 декабря 1895 года.";
+    private static final String MSG_ID_REQUIRED_VALIDATION_ERROR = "Id должен быть указан.";
 
     @GetMapping
     public Collection<Film> findAll() {
@@ -22,9 +26,12 @@ public class FilmController {
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
+        log.trace("input film:");
+        log.trace(film.toString());
         // проверяем выполнение необходимых условий
         if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза фильма не может быть раньше 28 декабря 1895 года.");
+            log.warn(MSG_RELEASEDATE_VALIDATION_ERROR);
+            throw new ValidationException(MSG_RELEASEDATE_VALIDATION_ERROR);
         }
 
         film.setId(getNextId());
@@ -35,13 +42,17 @@ public class FilmController {
 
     @PutMapping
     public Film update(@Valid @RequestBody Film newFilm) {
+        log.trace("input film:");
+        log.trace(newFilm.toString());
         // проверяем необходимые условия
         if (newFilm.getId() == null) {
-            throw new ValidationException("Id должен быть указан");
+            log.warn(MSG_ID_REQUIRED_VALIDATION_ERROR);
+            throw new ValidationException(MSG_ID_REQUIRED_VALIDATION_ERROR);
         }
         // проверяем выполнение необходимых условий
         if (newFilm.getReleaseDate() == null || newFilm.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            throw new ValidationException("Дата релиза фильма не может быть раньше 28 декабря 1895 года.");
+            log.warn(MSG_RELEASEDATE_VALIDATION_ERROR);
+            throw new ValidationException(MSG_RELEASEDATE_VALIDATION_ERROR);
         }
 
         if (films.containsKey(newFilm.getId())) {
@@ -54,6 +65,7 @@ public class FilmController {
             return oldFilm;
         }
 
+        log.warn("Фильм с id = " + newFilm.getId() + " не найден");
         throw new ValidationException("Фильм с id = " + newFilm.getId() + " не найден");
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
@@ -12,8 +13,13 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final Map<Long, User> users = new HashMap<>();
+    private static final String MSG_LOGIN_VALIDATION_ERROR = "Логин не может быть пустым и содержать пробелы.";
+    private static final String MSG_BIRTHDAY_VALIDATION_ERROR = "Дата рождения не может быть в будущем.";
+    private static final String MSG_ID_REQUIRED_VALIDATION_ERROR = "Id должен быть указан.";
+
 
     @GetMapping
     public Collection<User> findAll() {
@@ -22,15 +28,19 @@ public class UserController {
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
+        log.trace("input user:");
+        log.trace(user.toString());
         // проверяем выполнение необходимых условий
         if (user.getLogin() == null || user.getLogin().trim().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+            log.warn(MSG_LOGIN_VALIDATION_ERROR);
+            throw new ValidationException(MSG_LOGIN_VALIDATION_ERROR);
         }
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
         if (user.getBirthday() != null && user.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем.");
+            log.warn(MSG_BIRTHDAY_VALIDATION_ERROR);
+            throw new ValidationException(MSG_BIRTHDAY_VALIDATION_ERROR);
         }
 
         user.setId(getNextId());
@@ -41,16 +51,21 @@ public class UserController {
 
     @PutMapping
     public User update(@Valid @RequestBody User newUser) {
+        log.trace("input user:");
+        log.trace(newUser.toString());
         // проверяем необходимые условия
         if (newUser.getId() == null) {
-            throw new ValidationException("Id должен быть указан");
+            log.warn(MSG_ID_REQUIRED_VALIDATION_ERROR);
+            throw new ValidationException(MSG_ID_REQUIRED_VALIDATION_ERROR);
         }
         // проверяем выполнение необходимых условий
         if (newUser.getLogin() == null || newUser.getLogin().trim().contains(" ")) {
-            throw new ValidationException("Логин не может быть пустым и содержать пробелы.");
+            log.warn(MSG_LOGIN_VALIDATION_ERROR);
+            throw new ValidationException(MSG_LOGIN_VALIDATION_ERROR);
         }
         if (newUser.getBirthday() != null && newUser.getBirthday().isAfter(LocalDate.now())) {
-            throw new ValidationException("Дата рождения не может быть в будущем.");
+            log.warn(MSG_BIRTHDAY_VALIDATION_ERROR);
+            throw new ValidationException(MSG_BIRTHDAY_VALIDATION_ERROR);
         }
 
         if (users.containsKey(newUser.getId())) {
@@ -71,6 +86,7 @@ public class UserController {
             return oldUser;
         }
 
+        log.warn("Пользователь с id = " + newUser.getId() + " не найден");
         throw new ValidationException("Пользователь с id = " + newUser.getId() + " не найден");
     }
 
