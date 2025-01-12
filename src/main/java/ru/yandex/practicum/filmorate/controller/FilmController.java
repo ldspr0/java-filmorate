34493b0685e43d60a5db99182b92f9f6df00cc13
Al3x Ivanov <pro87@yandex.ru>
2025-01-12 22:01:19 +0400

    Added Maven and model to project

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,20 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
+@RequestMapping("/posts")
 public class FilmController {
+    private final Map<Long, Film> posts = new HashMap<>();
+
+
+    @GetMapping
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+
 }
